00c511647e9b1abbae0b1de410c48520cc6dd1a4
Luke Ferris <luke.ferris@gmail.com>
2023-07-19 20:41:42 +0200

    Added header suggestion

--- landingPageComponents/customSection.js
+++ landingPageComponents/customSection.js
@@ -7,6 +7,10 @@
 // - Make sure you add any required headings or labels to ensure the section is properly understood by the user
 // - Do not change the styling in the container div below.
 
+// STYLING:
+// - A header that explains the section should use typography variant h3
+// - Normal text content should use typography variant h2
+
 // You can assume Material UI (MUI) components and anything present as standard in CreateReactApp
 import { Typography } from "@mui/material";
 
